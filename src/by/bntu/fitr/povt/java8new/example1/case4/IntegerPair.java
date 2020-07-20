c1de16c67232e208867557d961d9263dbacc9b6e
Alexis-Dk <alexeydruzik@inbox.ru>
2020-07-20 11:37:59 +0300

    Added Lab1 from 'Information theory'.

---
+++ src/by/bntu/fitr/povt/java8new/example1/case4/IntegerPair.java
@@ -0,0 +1,14 @@
+package by.bntu.fitr.povt.java8new.example1.case4;
+
+/**
+ * @author Alexey Druzik on 24.06.2020
+ */
+public class IntegerPair {
+    int x;
+    int y;
+
+    IntegerPair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
