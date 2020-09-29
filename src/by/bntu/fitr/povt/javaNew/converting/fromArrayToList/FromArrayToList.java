fa91b995147e3603a61afa8295eb7421d02eae40
Alexis-Dk <alexeydruzik@inbox.ru>
2020-09-29 14:04:50 +0300

    Added example for converting array to list.

---
+++ src/by/bntu/fitr/povt/javaNew/converting/fromArrayToList/FromArrayToList.java
@@ -0,0 +1,15 @@
+package by.bntu.fitr.povt.javaNew.converting.fromArrayToList;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Alexey Druzik on 29.09.2020
+ */
+public class FromArrayToList {
+
+    public static void main(String[] args) {
+        String a1[] = new String[] { "A", "B", "C", "D" };
+        List list = Arrays.asList(a1);
+    }
+}
