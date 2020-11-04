c14dd5a3d08ba7485c546d0d5aae466b7c1d7eb4
Alexis-Dk <alexeydruzik@inbox.ru>
2020-11-04 18:28:04 +0300

    Added example with Arrays.asList.

---
+++ src/by/bntu/fitr/povt/immutable/case4/Test.java
@@ -0,0 +1,36 @@
+package by.bntu.fitr.povt.immutable.case4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Alexey Druzik on 04.11.2020
+ */
+public class Test {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        Collections.addAll(list, "values");
+        System.out.println(list);
+
+        /*
+            Arrays.asList returns a mutable list, but you can't add or remove an element(not be structurally modified)
+         */
+        List<String> list2 = Arrays.asList("1", "2");
+        list2.add("3");
+        for (String s: list2) {
+            System.out.println(s);
+        }
+
+        /*
+            If you really want to make a mutable list immutable, please try using Collections.unmodifiableList(mutableList);
+         */
+        List<String> list4 = new ArrayList<>();
+        list4.add("4");
+        List<String> list5 = Collections.unmodifiableList(list4);
+        for (String s: list5) {
+            System.out.println(s);
+        }
+    }
+}
