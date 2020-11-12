2246f2b3a11c84312fb61c3ba933b4c3c114ab1a
Alexis-Dk <alexeydruzik@inbox.ru>
2020-11-12 13:56:13 +0300

    Refactoring.

--- src/by/bntu/fitr/povt/javaNew/jdk10/case1LocalVariableTypeInterface/Main.java
+++ src/by/bntu/fitr/povt/javaNew/jdk10/case1LocalVariableTypeInterface/Main.java
@@ -1,6 +1,7 @@
 package by.bntu.fitr.povt.javaNew.jdk10.case1LocalVariableTypeInterface;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * @author Alexey Druzik on 12.11.2020
@@ -9,6 +10,8 @@ public class Main {
     public static void main(String[] args) {
 
         var list = new ArrayList<String>(); //перед нами ArrayList<String>
+        Collections.addAll(list, "Alex");
         var stream = list.stream(); // перед нами Stream<String>
+        stream.forEach(System.out::println);
     }
 }
