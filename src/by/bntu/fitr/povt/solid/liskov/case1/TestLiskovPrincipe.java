324ffab71487e1e40273e373c7cd5f9dfec91d2a
Alexis-Dk <alexeydruzik@inbox.ru>
2021-05-14 21:08:32 +0300

    Added comments.

--- src/by/bntu/fitr/povt/solid/liskov/case1/TestLiskovPrincipe.java
+++ src/by/bntu/fitr/povt/solid/liskov/case1/TestLiskovPrincipe.java
@@ -2,6 +2,10 @@ package by.bntu.fitr.povt.solid.liskov.case1;
 
 public class TestLiskovPrincipe {
 
+    /**
+     * Here is example of violation of the Liskov-principle, because child CarWrapper throws an
+     * exception but parent class DefaultCar - doesn't:
+     * */
     public static void main(String[] args) {
         Car car = new NCar();
         CarWrapper carWrapper = new CarWrapper();
